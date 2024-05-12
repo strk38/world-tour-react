20224496a4a50e398c7f5e6b5115e5bfea83feba
strk38 <tahmid.nizam.ibnul@g.bracu.ac.bd>
2024-05-12 18:38:38 +0600

    12-May-2024

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/country";
+import './Countries.css'
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
+    const handleVisitedCountry = country => {
+        console.log('add this to visited Country');
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    return (
+        <div>
+            <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited Countries {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name?.common}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="country-container">
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountry={handleVisitedCountry}
+                        country={country}></Country>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './Country.css'
+const Country = ({ country, handleVisitedCountry }) => {
+    //console.log(country)
+    const { name, flags, population, area, cca3 } = country;
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+    // console.log(handleVisitedCountry);
+
+    const passWithParams = () => {
+        handleVisitedCountry(country);
+    }
+
+    return (
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: visited ? 'Purple' : 'black' }}>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={passWithParams}>Mark Visited</button><br></br>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            {visited && ' I have visited this country'}
+        </div >
+    );
+};
+
+export default Country;
